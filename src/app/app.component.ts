cb05540dcbe48f27570f3de969bc38670d9e8211
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-04-28 18:30:43 +0200

    Instalo ngx-translate y http-loader. Los declaro en el app.module. Creo la factory. Y establezco los defaultLanguages en app.component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+
+
+  constructor(
+    public translate: TranslateService
+  ) {
+    translate.addLangs(['en', 'es']);
+    const lang = translate.getBrowserLang()
+    if( (lang !== 'es') && ( lang !== 'en') ){
+      translate.setDefaultLang('en');
+    }
+
+    
+    
+    
+  }
   title = 'angular-internationalization-tutorial';
 }
